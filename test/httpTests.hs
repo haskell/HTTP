18d4b6f2794256fb7877b80aaadac1628514234b
Tom Lokhorst <tom@lokhorst.eu>
2011-08-15 00:34:54 +0100

    Removed most warnings from test file. Check if server returned 200 status code

--- test/httpTests.hs
+++ test/httpTests.hs
@@ -1,59 +1,57 @@
 import Control.Exception
 
 import Data.Functor
-import Data.Maybe
 
-import System
-import System.Posix.Types (ProcessID)
 import System.Posix.Unistd (sleep)
-import System.Posix.Process (forkProcess, exitImmediately)
+import System.Posix.Process (forkProcess)
 import System.Posix.Signals (sigTERM, signalProcess)
 
 import qualified Network.Shed.Httpd as Httpd
 
 import Network.HTTP
-import Network.URI ( parseURI, uriPath )
+import Network.Stream (Result)
+import Network.URI (uriPath)
 
 import Test.Framework (defaultMain, testGroup)
 import Test.Framework.Providers.HUnit
 import Test.HUnit
 
--- Our local HTTP server will listen for connections on this port.
-portNum = 8080
+basicGetRequest :: Assertion
+basicGetRequest = do
+  response <- simpleHTTP (getRequest (testUrl "/basic/get"))
+  code <- getResponseCode response
+  assertEqual "HTTP status code" (2, 0, 0) code
+  body <- getResponseBody response
+  assertEqual "Receiving expected response" "It works." body
 
--- We'll send our requests to this URL
-testURL path = "http://localhost:" ++ show portNum ++ path
-
--- Fork a process to run an HTTP server, using the argument function
--- to process requests and issue responses.
-startServer :: (Httpd.Request -> IO Httpd.Response) -> IO ProcessID
-startServer processRequest = forkProcess (() <$ Httpd.initServer portNum processRequest)
-
--- Kill the server process, rather brutally.
-stopServer :: ProcessID -> IO ()
-stopServer pid = signalProcess sigTERM pid
-
--- Process a single request
+processRequest :: Httpd.Request -> IO Httpd.Response
 processRequest req = do
   case (Httpd.reqMethod req, Network.URI.uriPath (Httpd.reqURI req)) of 
-    ("GET", "/test")    -> return $ Httpd.Response 200 [] "It works."
-    _                   -> return $ Httpd.Response 500 [] "Unknown request"
+    ("GET", "/basic/get") -> return $ Httpd.Response 200 [] "It works."
+    _                     -> return $ Httpd.Response 500 [] "Unknown request"
 
-basicGetRequest :: Assertion
-basicGetRequest = do 
-    response <- simpleHTTP (getRequest (testURL "/test"))
-    body <- getResponseBody response
-    assertEqual "Receiving expected response" "It works." body
-    
-tests = [
-  testGroup "Basic tests" [
-     testCase "Basic GET request" basicGetRequest
-     ]
+getResponseCode :: Result (Response a) -> IO ResponseCode
+getResponseCode (Left err) = fail (show err)
+getResponseCode (Right r)  = return (rspCode r)
+
+tests =
+  [ testGroup "Basic tests"
+    [ testCase "Basic GET request" basicGetRequest
+    ]
   ]
+
+portNum :: Int
+portNum = 8080
+
+testUrl :: String -> String
+testUrl p = "http://localhost:" ++ show portNum ++ p
 
 main :: IO ()
 main = do
-  pid <- startServer processRequest
-  sleep 1 -- Give the server time to start :-(
+  pid <- forkProcess (() <$ Httpd.initServer portNum processRequest)
+  _   <- sleep 1 -- Give the server time to start :-(
   finally (defaultMain tests)
           (stopServer pid)
+  where
+    stopServer = signalProcess sigTERM
+
