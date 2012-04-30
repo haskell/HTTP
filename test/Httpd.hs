100395bfa106d40ec291958e0ea1f81b0811ccce
Ganesh Sittampalam <ganesh@earth.li>
2012-04-30 22:51:03 +0100

    put Shed-specific logic in one function

--- test/Httpd.hs
+++ test/Httpd.hs
@@ -6,6 +6,7 @@ module Httpd
     where
 
 import Control.Applicative
+import Control.Monad
 import Network.URI ( URI )
 
 import qualified Network.Shed.Httpd as Shed
@@ -13,23 +14,38 @@ import qualified Network.Shed.Httpd as Shed
     , reqMethod, reqURI, reqHeaders, reqBody
     )
 
-type Request = Shed.Request
-type Response = Shed.Response
+data Request = Request
+    {
+     reqMethod :: String,
+     reqURI :: URI,
+     reqHeaders :: [(String, String)],
+     reqBody :: String
+    }
+
+data Response = Response
+    {
+     respStatus :: Int,
+     respHeaders :: [(String, String)],
+     respBody :: String
+    }
 
 mkResponse :: Int -> [(String, String)] -> String -> Response
-mkResponse = Shed.Response
+mkResponse = Response
 
 initServer :: Int -> (Request -> IO Response) -> IO ()
-initServer port handler = () <$ Shed.initServer port handler
+initServer port handler =
+    () <$ Shed.initServer
+           port
+           (liftM responseToShed . handler . requestFromShed)
+  where
+     responseToShed (Response status hdrs body) =
+         Shed.Response status hdrs body
+     requestFromShed request =
+         Request
+         {
+          reqMethod = Shed.reqMethod request,
+          reqURI = Shed.reqURI request,
+          reqHeaders = Shed.reqHeaders request,
+          reqBody = Shed.reqBody request
+         }
 
-reqMethod :: Request -> String
-reqMethod = Shed.reqMethod
-
-reqURI :: Request -> URI
-reqURI = Shed.reqURI
-
-reqHeaders :: Request -> [(String, String)]
-reqHeaders = Shed.reqHeaders
-
-reqBody :: Request -> String
-reqBody = Shed.reqBody
