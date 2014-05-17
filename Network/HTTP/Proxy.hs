haskell/HTTP

commit dc58d34984a3aa2b251c5de5ab061f5a929a7e63

Fix warning on non-Windowss

Network/HTTP/Proxy.hs
@@ -21,7 +21,9 @@ module Network.HTTP.Proxy
 
 import Control.Monad ( when, mplus, join, liftM2)
 
+#if defined(WIN32)
 import Network.HTTP.Base ( catchIO )
+#endif
 import Network.HTTP.Utils ( dropWhileTail, chopAtDelim )
 import Network.HTTP.Auth
 import Network.URI

commit 673e0f58d513db5d7c7c44af530b88f324f723b1

Fix Windows build (broken by dc58d349)

Network/HTTP/Proxy.hs
@@ -19,6 +19,12 @@ module Network.HTTP.Proxy
        , parseProxy  -- :: String -> Maybe Proxy
        ) where
 
+{-
+#if !defined(WIN32) && defined(mingw32_HOST_OS)
+#define WIN32 1
+#endif
+-}
+
 import Control.Monad ( when, mplus, join, liftM2)
 
 #if defined(WIN32)